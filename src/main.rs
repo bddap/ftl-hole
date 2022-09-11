d8e5d870ac43b29fa1c9602d56d1c7f34d09b332
Andrew Dirksen <andrew@dirksen.com>
2022-09-11 16:25:26 -0700

    orbit calc is slightly incorrect but I removed the old version of nalgebra that was breaking wasm support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 mod orbit;
 
-use macroquad::prelude::*;
+use std::ops::Range;
 
-const TICKS_PER_SECOND: usize = 1000;
-const TICK_DURATION: f64 = 1.0 / TICKS_PER_SECOND as f64;
+use glam::{dvec2, DMat3, DVec2};
+use macroquad::{
+    color::colors,
+    prelude::{
+        clear_background, draw_circle, get_time, next_frame, screen_height, screen_width, BLACK,
+        WHITE, YELLOW,
+    },
+};
+
 const WORLD_RADIUS_METERS: f64 = 1024.0;
-const BLACK_HOLE_MASS: f64 = 5.97219_e14;
+const BLACK_HOLE_MASS: f64 = 5.97219_e17;
 const GRAVITATIONAL_CONSTANT: f64 = 6.67_e-11;
 const PULL: f64 = (BLACK_HOLE_MASS * GRAVITATIONAL_CONSTANT) as f64;
 
@@ -23,34 +30,38 @@ struct Satellite {
 
 impl Satellite {
     fn pos_at(&self, t: f64) -> DVec2 {
-        let csv = orbit::Csv::new(
-            self.pos.extend(0.0),
-            self.vel.extend(0.0),
-            orbit::CB::new(PULL).into(),
-        );
-        let koe = orbit::Koe::from_csv(csv);
-        let koe = koe.tick(t - self.when);
-        let csv = orbit::Csv::from_koe(koe);
-        dvec2(csv.r.x, csv.r.y)
+        let csv = orbit::Csv::new(self.pos.extend(0.0), self.vel.extend(0.0));
+        let koe = orbit::Koe::from_csv(csv, PULL);
+        let koe = koe.tick(t - self.when, PULL);
+        let csv = orbit::Csv::from_koe(koe, PULL);
+        dvec2(csv.pos.x, csv.pos.y)
+    }
+
+    fn period(&self) -> f64 {
+        let csv = orbit::Csv::new(self.pos.extend(0.0), self.vel.extend(0.0));
+        let koe = orbit::Koe::from_csv(csv, PULL);
+        koe.period(PULL)
     }
 }
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
     let warp_points = [
-        dvec2(1.0, -1.0),
-        dvec2(1.0, 1.0),
-        dvec2(-1.0, 1.0),
-        dvec2(-1.0, -1.0),
+        (dvec2(1.0, -1.0), colors::BEIGE),
+        (dvec2(1.0, 1.0), colors::DARKBROWN),
+        (dvec2(-1.0, 1.0), colors::DARKBROWN),
+        (dvec2(-1.0, -1.0), colors::DARKBROWN),
     ]
-    .map(|p| p * WORLD_RADIUS_METERS / 2.0);
+    .map(|(p, c)| (p * WORLD_RADIUS_METERS / 2.0, c));
 
-    let pos = dvec2(1.0, 0.0) * WORLD_RADIUS_METERS / 1.3;
+    let pos = dvec2(1.0, 0.0) * WORLD_RADIUS_METERS / 3.0;
     let v_mag = (PULL / pos.length()).sqrt(); // velocity for a circular orbit
     let player = Player {
         sat: Satellite {
             pos,
-            vel: dvec2(0.0, v_mag),
+            // bug?: any larger than 1.414213562 (sqrt(2)), and the orbit doesn't display
+            // dvec2(0.0, v_mag * 1.4142135623)
+            vel: dvec2(0.0, v_mag * 1.2),
             when: 0.0,
         },
     };
@@ -69,6 +80,13 @@ async fn main() {
 
         clear_background(BLACK);
 
+        draw_circle(
+            screen_width() / 2.0,
+            screen_height() / 2.0,
+            20.0,
+            colors::VIOLET,
+        );
+
         let player_pos_screen = world_to_screen * player.sat.pos_at(time).extend(1.0);
         draw_circle(
             player_pos_screen.x as f32,
@@ -77,9 +95,11 @@ async fn main() {
             YELLOW,
         );
 
-        for i in 0..100 {
-            let projected_pos_screen =
-                world_to_screen * player.sat.pos_at(time + i as f64).extend(1.0);
+        let points = 100;
+        let period = player.sat.period();
+        for i in 0..points {
+            let t = (i as f64).remap(0.0..((points - 1) as f64), 0.0..period);
+            let projected_pos_screen = world_to_screen * player.sat.pos_at(t).extend(1.0);
             draw_circle(
                 projected_pos_screen.x as f32,
                 projected_pos_screen.y as f32,
@@ -88,11 +108,35 @@ async fn main() {
             );
         }
 
-        for point in warp_points {
+        for (point, color) in warp_points {
             let pos_screen = world_to_screen * point.extend(1.0);
-            draw_circle(pos_screen.x as f32, pos_screen.y as f32, 15.0, DARKBROWN);
+            draw_circle(pos_screen.x as f32, pos_screen.y as f32, 15.0, color);
         }
 
         next_frame().await
+    }
+}
+
+trait Remap: Sized {
+    fn remap(self, current: Range<Self>, target: Range<Self>) -> Self;
+}
+
+impl Remap for f64 {
+    fn remap(self, current: Range<Self>, target: Range<Self>) -> Self {
+        (self - current.start) / (current.end - current.start) * (target.end - target.start)
+            + target.start
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn remap() {
+        assert_eq!(0.0.remap(0.0..1.0, 0.0..1.0), 0.0);
+        assert_eq!(1.0.remap(0.0..1.0, 0.0..1.0), 1.0);
+        assert_eq!(0.5.remap(0.0..1.0, 0.0..2.0), 1.0);
+        assert_eq!((-0.5).remap(0.0..-1.0, 0.0..2.0), 1.0);
     }
 }
--- src/orbit.rs
+++ src/orbit.rs
@@ -1,198 +1,165 @@
 // special thanks to nchashch who provided https://github.com/nchashch/orbital-mechanics-rust
 // on which this was originally based
 
-use macroquad::prelude::glam;
-use nalgebra::*;
-use std::f64::consts::*;
-use std::rc::*;
+use glam::{dvec3, DMat3, DVec3};
+use std::{f64::consts::PI, ops::Div};
 
 /// # Keplerian Orbital Elements
+///
 /// This structure represents an orbit using
 /// six keplerian element. It also holds mean motion
 /// to avoid recomputing it for every tick() call, and
 /// rot matrix to avoid recomputing it for every CSV::from_koe() call.
 /// Like CSV it holds a reference to the central body.
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Koe {
-    /// Semi Major Axis.
-    pub a: f64,
-    /// Eccentricity.
-    pub e: f64,
-    /// Inclination.
-    pub inc: f64,
+    pub semi_major_axis: f64,
+    pub eccentricity: f64,
+    pub inclination: f64,
     /// Longitude of Ascending Node.
     pub lan: f64,
     /// Argument of Periapsis.
     pub ap: f64,
-    /// Mean anomaly.
-    pub m0: f64,
-    /// Mean motion.
-    pub n: f64,
-    /// A matrix that transforms a vector lying in i, j plane into a corresponding vector lying in the orbital plane
-    /// defined by inc and lan, it is stored in KOE to avoid recomputing it for every CSV::from_koe() call.
-    pub rot: Rot3<f64>,
-    /// Reference to the central body.
-    pub cb: Rc<CB>,
+    pub mean_anomaly: f64,
 }
 
 impl Koe {
-    pub fn tick(&self, dt: f64) -> Self {
+    /// mu is a standard gravitational parameter Mass * Universal gravitational constant
+    pub fn period(&self, mu: f64) -> f64 {
+        self.semi_major_axis.powf(3.0).div(mu).sqrt() * PI * 2.0
+    }
+
+    /// mu is a standard gravitational parameter Mass * Universal gravitational constant
+    pub fn tick(&self, dt: f64, mu: f64) -> Self {
+        let mean_motion = (mu / self.semi_major_axis.powf(3.0)).sqrt();
         Koe {
-            m0: self.m0 + self.n * dt,
-            cb: self.cb.clone(),
+            mean_anomaly: self.mean_anomaly + mean_motion * dt,
             ..*self
         }
     }
 
-    /// Construct KOE from orbital elements and CB reference.
-    pub fn new(a: f64, e: f64, inc: f64, lan: f64, ap: f64, m0: f64, cb: Rc<CB>) -> Koe {
-        // Vector cb.i points towards intersection of 0th meridian and equator
-        // Vector cb.k points towards north pole
-        // cb.j == cross(&cb.k, &cb.i)
-
-        // rot transformation matrix is not expected to change very often
-        // and it is stored in KOE to avoid recomputing it
-        // for every CSV::from_koe() call
-        let mut rot = Rot3::new_identity(3);
-        // Do lan and inc rotations only if
-        // the orbit is not equatorial
-        if !approx_eq(&inc, &0.0) {
-            let lan_axisangle = cb.k * lan;
-            let inc_axisangle = cb.i * inc;
-            rot = rot.prepend_rotation(&lan_axisangle);
-            rot = rot.prepend_rotation(&inc_axisangle);
-        }
-        // Do ap rotation only if
-        // the orbit is not circular
-        if !approx_eq(&e, &0.0) {
-            let ap_axisangle = cb.k * ap;
-            rot = rot.prepend_rotation(&ap_axisangle);
-        }
-        // Mean motion
-        let n = (cb.mu / a.powf(3.0)).sqrt();
-        Koe {
-            a,
-            e,
-            inc,
-            lan,
-            ap,
-            m0,
-            n,
-            rot,
-            cb,
-        }
+    fn rot(&self) -> DMat3 {
+        DMat3::from_axis_angle(dvec3(0.0, 0.0, 1.0), self.ap)
     }
 
     /// Construct KOE from CSV.
-    pub fn from_csv(csv: Csv) -> Koe {
-        // Vector csv.cb.i points towards intersection of 0th meridian and equator
-        // Vector csv.cb.k points towards north pole
-        // csv.cb.j == cross(&csv.cb.k, &csv.cb.i)
+    ///
+    /// mu is a standard gravitational parameter: Mass of parent body * Universal gravitational constant
+    pub fn from_csv(csv: Csv, mu: f64) -> Koe {
+        let position = csv.pos;
+        let velocity = csv.vel;
 
-        // Radius vector
-        let r = csv.r;
-        // Velocity
-        let v = csv.v;
-
-        // Standard gravitational parameter
-        let mu = &csv.cb.mu;
-
-        // Specific angular momentum
-        let h = cross(&r, &v);
+        let specific_angular_momentum = position.cross(velocity);
 
         // Eccentricity vector
         // (It is pointed towards periapsis and it's length
         // is equal to eccentricity of the orbit)
-        let e = cross(&v, &h) / *mu - normalize(&r);
+        let ev = velocity.cross(specific_angular_momentum) / mu - position.normalize();
+        let eccentricity = ev.length();
 
         // Node vector
         // (vector pointing towards the ascending node)
         // Ascending node is a point where satelite
         // is above equator and goes north
-        let n = cross(&csv.cb.k, &h);
+        let acending_node = DVec3::Z.cross(specific_angular_momentum);
 
-        let cos_inc = dot(&h, &csv.cb.k) / (norm(&h));
+        let cos_inc = specific_angular_momentum.dot(DVec3::Z) / specific_angular_momentum.length();
         // cos_inc is sometimes greater than 1.0
         // and without this fix cos_inc.acos() is NaN
         // for cos_inc > 1.0 cases
-        let inc = if cos_inc > 1.0 { 0.0 } else { cos_inc.acos() };
-
-        // Eccentricity
-        let es = norm(&e);
+        let inclination = if cos_inc > 1.0 { 0.0 } else { cos_inc.acos() };
 
         // Longitude of Ascending Node
         // (angle between vector csv.cb.i and ascending node)
-        let mut lan = if dot(&n, &csv.cb.j) >= 0.0 {
-            (dot(&n, &csv.cb.i) / norm(&n)).acos()
+        let mut lan = if acending_node.dot(DVec3::NEG_Y) >= 0.0 {
+            (acending_node.dot(DVec3::X) / acending_node.length()).acos()
         } else {
-            2.0 * PI - (dot(&n, &csv.cb.i) / norm(&n)).acos()
+            2.0 * PI - (acending_node.dot(DVec3::X) / acending_node.length()).acos()
         };
 
-        let right = cross(&h, &n);
+        let right = specific_angular_momentum.cross(acending_node);
         // Argument of periapsis
         // (angle between ascending node and periapsis)
-        let mut ap = if dot(&e, &right) >= 0.0 {
-            (dot(&n, &e) / (norm(&n) * norm(&e))).acos()
+        let mut ap = if ev.dot(right) >= 0.0 {
+            (acending_node.dot(ev) / (acending_node.length() * ev.length())).acos()
         } else {
-            2.0 * PI - (dot(&n, &e) / (norm(&n) * norm(&e))).acos()
+            2.0 * PI - (acending_node.dot(ev) / (acending_node.length() * ev.length())).acos()
         };
 
         // If the orbit is circular ap is 0.0
         // (ap doesn't make sense for circular orbits)
-        if approx_eq(&es, &0.0) {
+        if approx_eq(eccentricity, 0.0) {
             ap = 0.0;
         }
         // If the orbit is equatorial lan is 0.0
         // (lan doesn't make sense for equatorial orbits)
-        if approx_eq(&inc, &0.0) {
+        if approx_eq(inclination, 0.0) {
             lan = 0.0;
             // If it is equatorial, non circular orbit ap is Longitude of Periapsis
             // (angle between vector csv.cb.i and periapsis)
-            if !approx_eq(&es, &0.0) {
-                ap = if dot(&e, &csv.cb.j) >= 0.0 {
-                    (dot(&csv.cb.i, &e) / norm(&e)).acos()
+            if !approx_eq(eccentricity, 0.0) {
+                ap = if ev.dot(DVec3::NEG_Y) >= 0.0 {
+                    (DVec3::X.dot(ev) / ev.length()).acos()
                 } else {
-                    2.0 * PI - (dot(&csv.cb.i, &e) / norm(&e)).acos()
+                    2.0 * PI - (DVec3::X.dot(ev) / ev.length()).acos()
                 };
             }
         }
 
         // True anomaly
         // (angle between periapsis and radius vector)
-        let mut ta = if dot(&r, &v) >= 0.0 {
-            (dot(&e, &r) / (norm(&e) * norm(&r))).acos()
+        let mut ta = if position.dot(velocity) >= 0.0 {
+            (ev.dot(position) / (ev.length() * position.length())).acos()
         } else {
-            2.0 * PI - (dot(&e, &r) / (norm(&e) * norm(&r))).acos()
+            2.0 * PI - (ev.dot(position) / (ev.length() * position.length())).acos()
         };
 
-        if approx_eq(&es, &0.0) {
+        if approx_eq(eccentricity, 0.0) {
             // For circular equatorial orbit use longitude
             // (angle between vector csv.cb.i and radius vector)
-            if approx_eq(&inc, &0.0) {
-                ta = if dot(&csv.cb.i, &v) <= 0.0 {
-                    (dot(&csv.cb.i, &r) / (norm(&csv.cb.i) * norm(&r))).acos()
+            if approx_eq(inclination, 0.0) {
+                ta = if DVec3::X.dot(velocity) <= 0.0 {
+                    (DVec3::X.dot(position) / (DVec3::X.length() * position.length())).acos()
                 } else {
-                    2.0 * PI - (dot(&csv.cb.i, &r) / (norm(&csv.cb.i) * norm(&r))).acos()
+                    2.0 * PI
+                        - (DVec3::X.dot(position) / (DVec3::X.length() * position.length())).acos()
                 }
             // For circular non equatorial orbit use argument of latitude
             // (angle between ascending node and radius vector)
             } else {
-                ta = if dot(&n, &v) <= 0.0 {
-                    (dot(&n, &r) / (norm(&n) * norm(&r))).acos()
+                ta = if acending_node.dot(velocity) <= 0.0 {
+                    (acending_node.dot(position) / (acending_node.length() * position.length()))
+                        .acos()
                 } else {
-                    2.0 * PI - (dot(&n, &r) / (norm(&n) * norm(&r))).acos()
+                    2.0 * PI
+                        - (acending_node.dot(position)
+                            / (acending_node.length() * position.length()))
+                        .acos()
                 }
             }
         }
 
+        let aa = ((1.0 + eccentricity) / (1.0 - eccentricity)).sqrt();
+        debug_assert!(!aa.is_nan());
         // Eccentric anomaly (intermidiate step to compute mean anomaly)
-        let ea = 2.0 * ((ta / 2.0).tan() / ((1.0 + es) / (1.0 - es)).sqrt()).atan();
+        let ea = 2.0 * ((ta / 2.0).tan() / aa).atan();
+        debug_assert!(!ea.is_nan());
         // Mean anomaly (it is used because it changes linearly with time,
         // and for that reason is cheap to update)
-        let m0 = ea - es * ea.sin();
-        // Semi Major Axis
-        let a = 1.0 / (2.0 / norm(&r) - sqnorm(&v) / mu);
-        Koe::new(a, es, inc, lan, ap, m0, csv.cb.clone())
+        let mean_anomaly = ea - eccentricity * ea.sin();
+        debug_assert!(!mean_anomaly.is_nan());
+
+        let semi_major_axis = 1.0 / (2.0 / position.length() - velocity.length_squared() / mu);
+        debug_assert!(!semi_major_axis.is_nan());
+
+        Koe {
+            semi_major_axis,
+            eccentricity,
+            inclination,
+            lan,
+            ap,
+            mean_anomaly,
+        }
     }
 }
 
@@ -200,62 +167,46 @@ impl Koe {
 /// This structure represents an orbit using a
 /// radius vector and a velocity vector.
 /// It holds a reference to the central body.
-#[derive(Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct Csv {
-    /// Radius vector.
-    pub r: Vec3<f64>,
+    /// aka radius vector.
+    pub pos: DVec3,
     /// Velocity.
-    pub v: Vec3<f64>,
-    /// Reference to the central body.
-    pub cb: Rc<CB>,
+    pub vel: DVec3,
 }
 
 impl Csv {
     /// Construct CSV from position and velocity.
-    pub fn new(r: glam::DVec3, v: glam::DVec3, cb: Rc<CB>) -> Csv {
-        let r = Vec3 {
-            x: r.x,
-            y: r.y,
-            z: r.z,
-        };
-        let v = Vec3 {
-            x: v.x,
-            y: v.y,
-            z: v.z,
-        };
-        Csv { r, v, cb }
-    }
-
-    /// Construct CSV from position and velocity.
-    pub fn nu(r: Vec3<f64>, v: Vec3<f64>, cb: Rc<CB>) -> Csv {
-        Csv { r, v, cb }
+    pub fn new(r: DVec3, v: DVec3) -> Csv {
+        Csv { pos: r, vel: v }
     }
 
     /// Construct CSV from KOE.
-    pub fn from_koe(koe: Koe) -> Csv {
+    /// mu is a standard gravitational parameter Mass * Universal gravitational constant
+    pub fn from_koe(koe: Koe, mu: f64) -> Csv {
         // Mean anomaly
-        let m0 = koe.m0;
+        let m0 = koe.mean_anomaly;
         // Number of iterations for newton_raphson
         let iterations = 10;
         // Eccentric anomaly
-        let ea = Csv::newton_raphson(&m0, &koe.e, &iterations);
+        let ea = Csv::newton_raphson(&m0, &koe.eccentricity, &iterations);
         // True anomaly
         let ta = 2.0
-            * ((1.0 + koe.e).sqrt() * (ea / 2.0).sin())
-                .atan2((1.0 - koe.e).sqrt() * (ea / 2.0).cos());
+            * ((1.0 + koe.eccentricity).sqrt() * (ea / 2.0).sin())
+                .atan2((1.0 - koe.eccentricity).sqrt() * (ea / 2.0).cos());
         // Distance to the center of the central body
-        let dist = koe.a * (1.0 - koe.e * ea.cos());
+        let dist = koe.semi_major_axis * (1.0 - koe.eccentricity * ea.cos());
         // Radius vector in i, j plane
-        let mut r = (koe.cb.i * ta.cos() + koe.cb.j * ta.sin()) * dist;
+        let mut r = (DVec3::X * ta.cos() + DVec3::NEG_Y * ta.sin()) * dist;
         // Velocity in i, j plane
-        let mut v = (koe.cb.i * (-ea.sin())
-            + koe.cb.j * ((1.0 - koe.e.powf(2.0)).sqrt() * ea.cos()))
-            * ((koe.cb.mu * koe.a).sqrt() / dist);
+        let mut v = (DVec3::X * (-ea.sin())
+            + DVec3::NEG_Y * ((1.0 - koe.eccentricity.powf(2.0)).sqrt() * ea.cos()))
+            * ((mu * koe.semi_major_axis).sqrt() / dist);
         // Radius vector in orbital plane
-        r = koe.rot.transform(&r);
+        r = koe.rot() * r;
         // Velocity in orbital plane
-        v = koe.rot.transform(&v);
-        Csv::nu(r, v, koe.cb.clone())
+        v = koe.rot() * v;
+        Csv::new(r, v)
     }
 
     // Function that numerically solves Kepler's equation
@@ -268,56 +219,36 @@ impl Csv {
     }
 }
 
-/// #Central body
-/// It holds standard gravitational parameter (product of body's mass and the gravitational constant)
-/// and a local coordinate system associated with this body.
-pub struct CB {
-    /// Standard gravitational parameter.
-    pub mu: f64,
-    /// A unit vector pointing towards
-    /// the intersection of 0th meridian and equator.
-    pub i: Vec3<f64>,
-    /// j = cross(&k, &i)
-    pub j: Vec3<f64>,
-    /// A unit vector pointing towards the north pole.
-    pub k: Vec3<f64>,
+fn approx_eq(a: f64, b: f64) -> bool {
+    (a - b).abs() < 0.0000001
 }
 
-impl CB {
-    /// Create a new central body asserting that i, j, k vector basis yields an orthonormal right-handed coordinate system.
-    pub fn new(mu: f64) -> CB {
-        let i = Vec3 {
-            x: 1.0,
-            y: 0.0,
-            z: 0.0,
-        };
-        let k = Vec3 {
-            x: 0.0,
-            y: 0.0,
-            z: 1.0,
-        };
-        let j = cross(&k, &i);
+#[cfg(test)]
+mod tests {
+    use rand::{Rng, SeedableRng};
 
-        // Assert that the basis vectors are orthogonal
-        assert!(approx_eq(&dot(&i, &j), &0.0));
-        assert!(approx_eq(&dot(&i, &k), &0.0));
-        assert!(approx_eq(&dot(&j, &k), &0.0));
+    use super::*;
 
-        // Assert that the basis vectors all have length 1
-        assert!(approx_eq(&norm(&i), &1.0));
-        assert!(approx_eq(&norm(&j), &1.0));
-        assert!(approx_eq(&norm(&k), &1.0));
+    #[test]
+    fn period() {
+        const PARENT_MASS: f64 = 5.97219_e15;
+        const GRAVITATIONAL_CONSTANT: f64 = 6.67_e-11;
+        const MU: f64 = PARENT_MASS * GRAVITATIONAL_CONSTANT;
+        const SCALE: f64 = 1000.0;
 
-        // Assert that the vector basis is right handed
-        assert!(approx_eq(&cross(&i, &j), &k));
-        CB { mu, i, j, k }
-    }
-}
+        let mut rng = rand::rngs::SmallRng::seed_from_u64(0);
+        let mut randf = move || rng.gen::<f64>() * SCALE;
 
-fn glamtona(a: glam::DVec3) -> Vec3<f64> {
-    Vec3 {
-        x: a.x,
-        y: a.y,
-        z: a.z,
+        for _ in 0..100 {
+            let csv = Csv {
+                pos: dvec3(randf(), randf(), randf()),
+                vel: dvec3(randf(), randf(), randf()),
+            };
+            let koe = Koe::from_csv(csv, MU);
+            let new_csv = Csv::from_koe(koe.tick(koe.period(MU), MU), MU);
+            dbg!(csv, new_csv, koe.period(MU));
+            assert!(approx_eq((csv.pos - new_csv.pos).length(), 0.0));
+            assert!(approx_eq((csv.vel - new_csv.vel).length(), 0.0));
+        }
     }
 }
