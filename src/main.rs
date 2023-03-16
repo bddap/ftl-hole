507dc0b762df97eff51d2df14ec8f002a224384b
Andrew Dirksen <andrew@dirksen.com>
2023-03-15 17:40:48 -0700

    achieve prototype

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,51 @@
-mod orbit;
-
 use std::ops::Range;
 
-use glam::{dvec2, DMat3, DVec2};
+use glam::{dvec2, DMat3, DVec2, Vec3Swizzles};
+use itertools::Itertools;
 use macroquad::{
     color::colors,
     prelude::{
-        clear_background, draw_circle, get_time, next_frame, screen_height, screen_width, vec2,
-        Color, WHITE, YELLOW,
+        clear_background, draw_circle, draw_line, get_time, is_mouse_button_pressed,
+        mouse_position, next_frame, screen_height, screen_width, vec2, MouseButton, YELLOW,
     },
-    ui::root_ui,
 };
 
 const WORLD_RADIUS_METERS: f64 = 1024.0;
 const BLACK_HOLE_MASS: f64 = 5.97219_e17;
 const GRAVITATIONAL_CONSTANT: f64 = 6.67_e-11;
-const PULL: f64 = (BLACK_HOLE_MASS * GRAVITATIONAL_CONSTANT) as f64;
+const PULL: f64 = BLACK_HOLE_MASS * GRAVITATIONAL_CONSTANT;
 
 struct Player {
-    sat: Satellite,
+    sat: Sat,
 }
 
-#[derive(Clone, Copy)]
-struct Satellite {
+#[derive(Clone, Copy, Debug)]
+struct Sat {
     pos: DVec2,
     vel: DVec2,
     // when were we at this pos and vel
     when: f64,
 }
 
-impl Satellite {
-    fn pos_at(&self, t: f64) -> DVec2 {
-        let csv = orbit::Csv::new(self.pos.extend(0.0), self.vel.extend(0.0));
-        let koe = orbit::Koe::from_csv(csv, PULL);
-        let koe = koe.tick(t - self.when, PULL);
-        let csv = orbit::Csv::from_koe(koe, PULL);
-        dvec2(csv.pos.x, csv.pos.y)
+impl Sat {
+    fn tick_to(&mut self, when: f64) {
+        let dt = 0.001;
+        while self.when < when {
+            let acc = self.acceleration();
+            self.vel += acc * dt;
+            self.pos += self.vel * dt;
+            self.when += dt;
+        }
     }
 
-    fn period(&self) -> f64 {
-        let csv = orbit::Csv::new(self.pos.extend(0.0), self.vel.extend(0.0));
-        let koe = orbit::Koe::from_csv(csv, PULL);
-        koe.period(PULL)
+    fn acceleration(&self) -> DVec2 {
+        let r = self.pos.length();
+        let r3 = r * r * r;
+        -self.pos * PULL / r3
     }
 }
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("ftl-hole")]
 async fn main() {
     let warp_points = [
         (dvec2(1.0, -1.0), colors::BEIGE),
@@ -55,39 +55,22 @@ async fn main() {
     ]
     .map(|(p, c)| (p * WORLD_RADIUS_METERS / 2.0, c));
 
-    let vs_max: f32 = 2.0f32.sqrt() - 0.000001;
-    let vs_min: f32 = 0.5;
-    let mut initial_radius: f32 = 1.0 / 6.0;
+    let initial_radius: f32 = 1.0 / 6.0;
+    let pos = dvec2(initial_radius as f64, 0.0) * WORLD_RADIUS_METERS;
+    let v_scale = 0.8;
+    let v_mag = (PULL / pos.length()).sqrt();
+    let mut player = Player {
+        sat: Sat {
+            pos,
+            vel: dvec2(0.0, v_scale * v_mag),
+            when: 0.0,
+        },
+    };
 
     loop {
         let time = get_time();
 
         let screen_min_dim = screen_width().min(screen_height()) as f64;
-
-        root_ui().window(
-            12313,
-            vec2(0.0, 0.0),
-            vec2(screen_min_dim as f32 / 4.0, screen_min_dim as f32 / 15.0),
-            |ui| {
-                ui.drag(232342, "initial_r", Some((0.0, 1.0)), &mut initial_radius);
-            },
-        );
-
-        let v_scale = (time / 32.0)
-            .sin()
-            .remap(-1.0..1.0, vs_min as f64..vs_max as f64) as f32;
-
-        let pos = dvec2(initial_radius as f64, 0.0) * WORLD_RADIUS_METERS;
-        let v_mag = (PULL / pos.length()).sqrt(); // velocity for a circular orbit
-        let player = Player {
-            sat: Satellite {
-                pos,
-                // bug?: any larger than 1.414213562 (sqrt(2)), and the orbit doesn't display
-                // dvec2(0.0, v_mag * 1.4142135623)
-                vel: dvec2(0.0, v_scale as f64 * v_mag),
-                when: 0.0,
-            },
-        };
 
         let world_to_screen = DMat3::from_translation(dvec2(
             screen_width() as f64 / 2.0,
@@ -96,6 +79,26 @@ async fn main() {
             screen_min_dim / 2.0 / WORLD_RADIUS_METERS,
             screen_min_dim / 2.0 / WORLD_RADIUS_METERS,
         ));
+        let screen_to_world = world_to_screen.inverse();
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            // find nearest warp point to the cursor
+            let mouse_pos = dvec2(mouse_position().0.into(), mouse_position().1.into());
+            let mouse_pos = (screen_to_world * mouse_pos.extend(1.0)).xy();
+
+            let warp_pos = warp_points
+                .iter()
+                .map(|(p, _)| p)
+                .copied()
+                .min_by_key(|p| (*p - mouse_pos).length_squared() as i64)
+                .unwrap();
+
+            dbg!(player.sat);
+            player.sat.pos = warp_pos;
+            dbg!(player.sat);
+        }
+
+        player.sat.tick_to(time);
 
         clear_background(colors::BLACK);
 
@@ -111,21 +114,21 @@ async fn main() {
             draw_circle(pos_screen.x as f32, pos_screen.y as f32, 15.0, color);
         }
 
-        let points = 1000;
-        let period = player.sat.period();
-        let dot_dur = period; //.min(60.0);
-        for i in 0..points {
+        let points = 32;
+        let dot_dur = 1.0;
+        let mut p = player.sat;
+        p.vel = -p.vel;
+        let point_poses = (0..points).map(|i| {
             let t = (i as f64).remap(0.0..((points - 1) as f64), 0.0..dot_dur);
-            let projected_pos_screen = world_to_screen * player.sat.pos_at(t).extend(1.0);
-            draw_circle(
-                projected_pos_screen.x as f32,
-                projected_pos_screen.y as f32,
-                2.0,
-                WHITE,
-            );
+            p.tick_to(time + t);
+            let projected_pos_screen = world_to_screen * p.pos.extend(1.0);
+            vec2(projected_pos_screen.x as f32, projected_pos_screen.y as f32)
+        });
+        for (a, b) in point_poses.tuple_windows() {
+            draw_line(a.x, a.y, b.x, b.y, 2.0, YELLOW);
         }
 
-        let player_pos_screen = world_to_screen * player.sat.pos_at(time).extend(1.0);
+        let player_pos_screen = world_to_screen * player.sat.pos.extend(1.0);
         draw_circle(
             player_pos_screen.x as f32,
             player_pos_screen.y as f32,
